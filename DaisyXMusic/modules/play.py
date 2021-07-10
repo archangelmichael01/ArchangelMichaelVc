100e026b442432db4cdf6c72f9f4b5ab32e44475
RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2021-07-10 15:26:25 +0530

    ❤️
    Nobita Randi kaa

--- DaisyXMusic/modules/play.py
+++ DaisyXMusic/modules/play.py
@@ -542,7 +542,7 @@ async def play(_, message: Message):
         )
         file_name = get_file_name(audio)
         title = file_name
-        thumb_name = "https://telegra.ph/file/f6086f8909fbfeb0844f2.png"
+        thumb_name = "https://telegra.ph/file/083ea5dbe4002079b0642.jpg"
         thumbnail = thumb_name
         duration = round(audio.duration / 60)
         views = "Locally added"
@@ -963,7 +963,7 @@ async def deezer(client: Client, message_: Message):
         url = songs.result[0].url
         artist = songs.result[0].artist
         duration = songs.result[0].duration
-        thumbnail = "https://telegra.ph/file/f6086f8909fbfeb0844f2.png"
+        thumbnail = "https://telegra.ph/file/083ea5dbe4002079b0642.jpg"
 
     except:
         await res.edit("Found Literally Nothing, You Should Work On Your English!")
